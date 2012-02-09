857e3ed728055f9930ace8ae5a01a5283f628988
Siri Hedengran <siricat@gmail.com>
2012-02-08 17:01:42 -0800

    implement adding tickets to a project, all tests passing

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,44 @@
+class TicketsController < ApplicationController
+  
+before_filter :find_project
+before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
+
+  def new
+    @ticket = @project.tickets.build
+  end
+  
+  def create
+    @ticket = @project.tickets.build(params[:ticket])
+    if @ticket.save
+      flash[:notice] = "Ticket has been created."
+      redirect_to [@project, @ticket]
+      # which is the project_ticket_path(@project, @ticket)
+      # projects/1/tickets/2 as an example
+    else
+      flash[:alert] = "Ticket has not been created."
+      render :action => "new"
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+    def find_project
+      @project = Project.find(params[:project_id])
+    end
+    
+    def find_ticket
+      @ticket = @project.tickets.find(params[:id])
+    end
+    
+end
