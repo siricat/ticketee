89ae83edd87026eeb6241022f51bb0438aedc9c4
Siri Hedengran <siricat@gmail.com>
2012-02-27 16:27:13 -0800

    projects will not show up for users that don't have permission

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,5 +8,16 @@ class Project < ActiveRecord::Base
     joins(:permissions).where(:permissions => { :action => "view",
                                                 :user_id => user.id })
   }
+  
+  def find_project
+    @project = Project.for(current_user).find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
+    redirect_to projects_path
+  end
+  
+  def self.for(user)
+    user.admin? ? Project : Project.readable_by(user)
+  end
     
 end
