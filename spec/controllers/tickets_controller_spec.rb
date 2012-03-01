315b8258413928028d2be8abd42ce544d25a61a4
Siri Hedengran <siricat@gmail.com>
2012-02-29 16:25:23 -0800

    restrict destroy ticket action to only users with permission

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -52,8 +52,13 @@ describe TicketsController do
                     :ticket => {}
                     }
       cannot_update_tickets!
-      end      
+    end      
+    
+    it "cannot delete a ticket without permission" do
+      delete :destroy, { :project_id => project.id, :id => ticket.id }
+      response.should redirect_to(project)
+      flash[:alert].should eql("You cannot delete tickets from this project.")
+    end
     
   end
-
 end
