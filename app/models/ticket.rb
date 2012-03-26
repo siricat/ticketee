6e8b737da6c85f9b23ddfb22eb4c0cf3af8d312b
Siri Hedengran <siricat@gmail.com>
2012-03-26 15:42:56 -0700

    add ticket email notifications

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,10 +1,10 @@
 class Ticket < ActiveRecord::Base
 
   searcher do
-    label :tag, :from => :tags, :field => :name
+    label :tag, :from => :tags, :field => "name"
     label :state, :from => :state, :field => "name"
   end
-  
+    
   # can now search like this:
   # tag:name_of_tag, tag:iteration_1
   # state:name_of_state, state:Open
@@ -19,9 +19,15 @@ class Ticket < ActiveRecord::Base
   accepts_nested_attributes_for :assets
   has_many :comments
   has_and_belongs_to_many :tags
+  has_and_belongs_to_many :watchers, :join_table => "ticket_watchers",
+                                     :class_name => "User"
+                                     
+  after_create :creator_watches_me
   
   validates :title, :presence  => true
   validates :description, :presence  => true, :length => { :minimum => 10 }
+  
+
   
   def tag!(tags)
     tags = tags.split(" ").map do |tag|
@@ -29,5 +35,10 @@ class Ticket < ActiveRecord::Base
     end
     self.tags << tags
   end
+  
+  private
+    def creator_watches_me
+      self.watchers << user unless self.watchers.include?(user)
+    end
   
 end
